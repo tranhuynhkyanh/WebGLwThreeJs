4b91ace027f1e350e7b1169b83c8e2abd4230c79
Nguyễn Thành Công <58556683+Tfushimura@users.noreply.github.com>
2022-06-04 19:41:13 +0700

    Add files via upload

---
+++ animate/src/index.ts
@@ -0,0 +1,155 @@
+import { KeyDisplay } from './utils';
+import { CharacterControls } from './characterControls';
+import * as THREE from 'three'
+import { CameraHelper } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
+// SCENE
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8def0);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 5;
+camera.position.z = 5;
+camera.position.x = 0;
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.shadowMap.enabled = true
+
+// CONTROLS
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true
+orbitControls.minDistance = 5
+orbitControls.maxDistance = 15
+orbitControls.enablePan = false
+orbitControls.maxPolarAngle = Math.PI / 2 - 0.05
+orbitControls.update();
+
+// LIGHTS
+light()
+
+// FLOOR
+generateFloor()
+
+// MODEL WITH ANIMATIONS
+var characterControls: CharacterControls
+new GLTFLoader().load('models/Soldier.glb', function (gltf) {
+    const model = gltf.scene;
+    model.traverse(function (object: any) {
+        if (object.isMesh) object.castShadow = true;
+    });
+    scene.add(model);
+
+    const gltfAnimations: THREE.AnimationClip[] = gltf.animations;
+    const mixer = new THREE.AnimationMixer(model);
+    const animationsMap: Map<string, THREE.AnimationAction> = new Map()
+    gltfAnimations.filter(a => a.name != 'TPose').forEach((a: THREE.AnimationClip) => {
+        animationsMap.set(a.name, mixer.clipAction(a))
+    })
+
+    characterControls = new CharacterControls(model, mixer, animationsMap, orbitControls, camera,  'Idle')
+});
+
+// MODEL 1
+const loader = new GLTFLoader().setPath( 'models/pikachu/' );
+loader.load( 'pikachu.gltf', function ( gltf ) {
+    const model1 = gltf.scene;
+    model1.traverse(function (object: any) {
+        if (object.isMesh) object.castShadow = true;
+    });
+    model1.scale.set(0.5,0.5,0.5);
+    model1.position.setY(Math.PI/2 - 0.3);
+	scene.add( model1 );
+
+} );
+
+// CONTROL KEYS
+const keysPressed = {  }
+const keyDisplayQueue = new KeyDisplay();
+document.addEventListener('keydown', (event) => {
+    keyDisplayQueue.down(event.key)
+    if (event.shiftKey && characterControls) {
+        characterControls.switchRunToggle()
+    } else {
+        (keysPressed as any)[event.key.toLowerCase()] = true
+    }
+}, false);
+document.addEventListener('keyup', (event) => {
+    keyDisplayQueue.up(event.key);
+    (keysPressed as any)[event.key.toLowerCase()] = false
+}, false);
+
+const clock = new THREE.Clock();
+// ANIMATE
+function animate() {
+    let mixerUpdateDelta = clock.getDelta();
+    if (characterControls) {
+        characterControls.update(mixerUpdateDelta, keysPressed);
+    }
+    orbitControls.update()
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+document.body.appendChild(renderer.domElement);
+animate();
+
+// RESIZE HANDLER
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    keyDisplayQueue.updatePosition()
+}
+window.addEventListener('resize', onWindowResize);
+
+function generateFloor() {
+    // TEXTURES
+    const textureLoader = new THREE.TextureLoader();
+    const placeholder = textureLoader.load("./textures/placeholder/Wall.png");
+
+    const WIDTH = 80
+    const LENGTH = 80
+
+    const geometry = new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512);
+    placeholder.anisotropy = 32
+    placeholder.repeat.set(50, 50)
+    placeholder.wrapT = THREE.RepeatWrapping
+    placeholder.wrapS = THREE.RepeatWrapping
+    const material = new THREE.MeshPhongMaterial(
+        {
+            map: placeholder,
+        })
+    const floor = new THREE.Mesh(geometry, material)
+    floor.receiveShadow = true
+    floor.rotation.x = - Math.PI / 2
+    scene.add(floor)
+}
+
+function wrapAndRepeatTexture (map: THREE.Texture) {
+    map.wrapS = map.wrapT = THREE.RepeatWrapping
+    map.repeat.x = map.repeat.y = 10
+}
+
+function light() {
+    scene.add(new THREE.AmbientLight(0xffffff, 0.7))
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+    dirLight.position.set(60, 100, -60);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = - 50;
+    dirLight.shadow.camera.left = - 50;
+    dirLight.shadow.camera.right = 50;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.mapSize.width = 4096;
+    dirLight.shadow.mapSize.height = 4096;
+    scene.add(dirLight);
+    // scene.add( new THREE.CameraHelper(dirLight.shadow.camera))
+}
